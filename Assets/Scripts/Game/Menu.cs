bd91407186718681edc410ed6557ec1b030cc67c
Berzan <berzanbrix@gmail.com>
2021-05-30 18:29:04 +0200

    Partly commented Menu class
    not done

--- Assets/Scripts/Game/Menu.cs
+++ Assets/Scripts/Game/Menu.cs
@@ -1,9 +1,15 @@
-﻿using TMPro;
+﻿// Author: maka4519 - Maximiliam Rosén
+// Group 02: maka4519 - Maximiliam Rosén, vida6631 - Viktor Dahlberg, anbe5918 - Andreas Berzelius
+
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Game
 {
+    /// <summary>
+    /// The Menu class
+    /// </summary>
     public class Menu : MonoBehaviour
     {
         [SerializeField] private GameObject birdPrefab;
@@ -15,28 +21,29 @@ namespace Game
 
         private static bool _isGameOver;
         private static int _currentScore;
+        // Registers listening to event onBirdScored to know when to add score
         private void Start()
         {
             //Bird.OnBirdDiedEvent += GameOver;
             MLAgentBird.OnBirdScored += AddScore;
         }
-
+        // Sets the score text 'score' value
         private void AddScore(int score)
         {
             scoreUI.SetText(score.ToString());
         }
-
+        // Enables the game over text and sets the boolean _isGameOVer to true
         private void GameOver()
         {
             gameOverTextUI.SetActive(true);
             _isGameOver = true;
         }
-
+        // Restarts the game if it is game over
         public void Restart(InputAction.CallbackContext context)
         {
             if (_isGameOver) StartNewGame();
         }
-
+        // Starts a new game
         public void StartNewGame()
         {
             playGameUIButton.SetActive(false);
@@ -47,12 +54,12 @@ namespace Game
             pipeGenerator.Clear();
             SpawnBirds(totalBirds);
         }
-
+        // Get the amount of birds
         public int GetTotalBirds()
         {
             return totalBirds;
         }
-
+        // Spawns 'i' amount of birds
         private void SpawnBirds(int i)
         {
             for (var j = 0; j < i; j++)
